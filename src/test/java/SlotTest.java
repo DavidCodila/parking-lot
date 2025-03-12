41f95208c5a3d8ef3e9ff1dce0927027fc8483d1
David Codila <david.codila@auspost.com.au>
2025-03-12 13:38:14 +1100

    mock dependencies and refactor SlotTest class

--- src/test/java/SlotTest.java
+++ src/test/java/SlotTest.java
@@ -4,24 +4,23 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class SlotTest {
     private Slot slot;
-    private Slot slot2;
+    private static final int SLOT_NUMBER = 1;
 
+    @Mock private Slot slot2;
     @Mock private Car car;
     @Mock private Car car2;
 
-    private final int slotId = 1;
-
     @BeforeEach
     public void setUp() {
-        int slot2Id = 2;
-        this.slot = new Slot(this.slotId, 1);
-        this.slot2 = new Slot(slot2Id, 2);
+        this.slot = new Slot(SLOT_NUMBER, 1);
         this.slot.setNextSlot(this.slot2);
         this.slot.parkCar(this.car);
     }
@@ -33,8 +32,9 @@ public class SlotTest {
 
     @Test
     public void testSendsCarToNextSlot() {
+        doNothing().when(this.slot2).parkCar(any(Car.class));
         this.slot.parkCar(this.car2);
-        assertEquals(this.car2, this.slot2.getCar());
+        verify(this.slot2, times(1)).parkCar(any(Car.class));
     }
 
     @Test
@@ -46,9 +46,25 @@ public class SlotTest {
     }
 
     @Test
+    public void testUnParkCar() throws Exception {
+        String methodOutput = tapSystemOut(() -> this.slot.unParkCar(this.car)).trim();
+        assertEquals("Slot " + SLOT_NUMBER + " is free", methodOutput);
+        assertNull(this.slot.getCar());
+    }
+
+    @Test
+    public void testSendsUnParkCommandToNextSlot() {
+        doNothing().when(this.slot2).unParkCar(any(Car.class));
+        this.slot.unParkCar(this.car2);
+        verify(this.slot2, times(1)).unParkCar(any(Car.class));
+    }
+
+    @Test
     public void testUnParkCarWithACarThatDoesNotHaveASlot() {
+        this.slot.unParkCar(this.car);
+        this.slot.setNextSlot(null);
         var exception = assertThrows(RuntimeException.class,
-                () -> this.slot.unParkCar(this.car2));
+                () -> this.slot.unParkCar(this.car));
         assertEquals("That car does not has a slot", exception.getMessage());
     }
 }
