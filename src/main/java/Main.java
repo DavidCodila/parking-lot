a8bd4f811225213895a72e16cfc4954aa11b1b5d
David Codila <david.codila@auspost.com.au>
2025-03-25 10:14:00 +1100

    refactor Main to make it easier to read

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import application.ParkingLot;
-import command.*;
+import command.FindCarCommand;
+import command.ListCarCommand;
+import command.ParkCarCommand;
+import command.UnParkCarCommand;
 import command.interfaces.CommandInterface;
 
 import java.io.FileNotFoundException;
@@ -14,39 +17,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         final int MAX_CAPACITY = 5;
         ParkingLot parkingLot = new ParkingLot(MAX_CAPACITY);
-
-        List<String> commandLines;
         final String PATH_TO_COMMAND_FILE = "./src/main/resources/commands";
+
         try {
-            commandLines = Files.readAllLines(Path.of(PATH_TO_COMMAND_FILE));
+            Files.readAllLines(Path.of(PATH_TO_COMMAND_FILE))
+                    .stream()
+                    .map(Main::generateCommand)
+                    .toList()
+                    .forEach(command -> command.execute(parkingLot));
         } catch (IOException e) {
             throw new FileNotFoundException("File with path: " + PATH_TO_COMMAND_FILE + " not found");
         }
-
-        List<CommandInterface> commands = commandLines.stream()
-                .map(Main::generateCommand)
-                .toList();
-        commands.forEach(command -> command.execute(parkingLot));
     }
 
     private static CommandInterface generateCommand(String commandLine) {
-        final String PARK_CAR = "PARK_CAR";
-        final String FIND = "FIND";
-        final String UN_PARK_CAR = "UN_PARK_CAR";
-        final String LIST = "LIST";
-
-        final String DELIMITER = " ";
         final int PARAMETER_INDEX = 1;
-
-        List<String> split = Arrays.asList(commandLine.split(DELIMITER));
+        List<String> split = Arrays.asList(commandLine.split(" "));
         String command = split.getFirst();
         List<String> parameterLineSplit = split.subList(PARAMETER_INDEX, split.size());
 
         return switch (command) {
-            case PARK_CAR -> new ParkCarCommand(parameterLineSplit);
-            case FIND -> new FindCarCommand(parameterLineSplit);
-            case UN_PARK_CAR -> new UnParkCarCommand(parameterLineSplit);
-            case LIST -> new ListCarCommand(parameterLineSplit);
+            case "PARK_CAR" -> new ParkCarCommand(parameterLineSplit);
+            case "FIND" -> new FindCarCommand(parameterLineSplit);
+            case "UN_PARK_CAR" -> new UnParkCarCommand(parameterLineSplit);
+            case "LIST" -> new ListCarCommand(parameterLineSplit);
             default -> throw new InvalidParameterException("Command " + command + " is not valid");
         };
     }
