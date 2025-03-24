1bc5b7be82b921e037a65e04fd47ea9f7b8340c7
David Codila <david.codila@auspost.com.au>
2025-03-24 16:18:38 +1100

    unify ParkingLot fields into a single HashMap slotNumberToCarIdMap and alter tests accordingly

--- src/main/java/application/ParkingLot.java
+++ src/main/java/application/ParkingLot.java
@@ -3,64 +3,62 @@ package application;
 import com.google.common.annotations.VisibleForTesting;
 
 import java.security.InvalidParameterException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ParkingLot {
-    private final Map<Integer, Integer> carIDToSlotNumberMap = new HashMap<>();
-    private final List<Integer> availableSlotNumbers;
+    private final Map<Integer, Integer> slotNumberToCarIdMap;
 
     public ParkingLot(int maxCapacity) {
-        this.availableSlotNumbers = new ArrayList<>(maxCapacity);
+        this.slotNumberToCarIdMap = new HashMap<>(maxCapacity);
         for (int i = 0; i < maxCapacity; i++) {
-            this.availableSlotNumbers.add(i);
+            this.slotNumberToCarIdMap.put(i, null);
         }
     }
 
-    public void parkCar(int id) {
-        int slotNumber = this.availableSlotNumbers.stream()
-                .min(Integer::compareTo)
-                .orElseThrow(() ->
-                        new RuntimeException("Can not park car, parking lot is full")
-                );
-        System.out.printf("SLOT %d is allocated to %d\n", slotNumber, id);
-        this.availableSlotNumbers.removeIf(value -> value.equals(slotNumber));
-        this.carIDToSlotNumberMap.put(id, slotNumber);
+    public void parkCar(int carId) {
+        Integer slotNumber = this.slotNumberToCarIdMap.entrySet().stream()
+                .filter(entry -> entry.getValue() == null)
+                .min(Map.Entry.comparingByKey())
+                .map(Map.Entry::getKey)
+                .orElseThrow(() -> new RuntimeException("Can not park car, parking lot is full"));
+        this.slotNumberToCarIdMap.put(slotNumber, carId);
+        System.out.printf("SLOT %d is allocated to %d\n", slotNumber, carId);
     }
 
-    public void findCar(int id) {
-        this.validateCarIsInParkingLot(id);
-        System.out.printf("%d is parked at Slot number %d\n", id, this.carIDToSlotNumberMap.get(id));
+    public void findCar(int carId) {
+        System.out.printf("%d is parked at Slot number %d\n", carId, this.getSlotNumberFromCarID(carId));
     }
 
-    public void listCars(List<Integer> ids) {
-        ids.forEach(this::findCar);
+    public void listCars(List<Integer> carIds) {
+        carIds.forEach(this::findCar);
     }
 
-    public void unParkCar(int id) {
-        this.validateCarIsInParkingLot(id);
-        int slotNumber = this.carIDToSlotNumberMap.get(id);
-        this.availableSlotNumbers.add(slotNumber);
+    public void unParkCar(int carId) {
+        int slotNumber = this.getSlotNumberFromCarID(carId);
+        this.slotNumberToCarIdMap.put(slotNumber, null);
         System.out.printf("Slot %d is free\n", slotNumber);
-        this.carIDToSlotNumberMap.remove(id);
     }
 
-    private void validateCarIsInParkingLot(int id) {
-        if (!this.carIDToSlotNumberMap.containsKey(id)) {
-            throw new InvalidParameterException("Car with id: " + id + " could not be found");
-        }
+    private int getSlotNumberFromCarID(int carId) {
+        return this.slotNumberToCarIdMap.entrySet().stream()
+                .filter(entry -> entry.getValue() != null && entry.getValue() == carId)
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElseThrow(() -> new InvalidParameterException("Car with id: " + carId + " could not be found"));
     }
 
     @VisibleForTesting
-    Integer getCarIDToSlotNumberMap(int id) {
-        return this.carIDToSlotNumberMap.get(id);
+    Map.Entry <Integer, Integer> retrieveSlotNumberToCarIdEntry(int carId) {
+        return this.slotNumberToCarIdMap.entrySet().stream()
+                .filter(entry -> entry.getKey() == carId)
+                .findFirst()
+                .orElseThrow( () -> new NullPointerException("Car with id: " + carId + " could not be found"));
     }
 
     @VisibleForTesting
     void setCarIDToSlotNumber(int carId, int slotNumber) {
-        this.availableSlotNumbers.removeIf(value -> value.equals(slotNumber));
-        this.carIDToSlotNumberMap.put(carId, slotNumber);
+        this.slotNumberToCarIdMap.put(slotNumber, carId);
     }
 }
--- src/test/java/application/ParkingLotTest.java
+++ src/test/java/application/ParkingLotTest.java
@@ -11,10 +11,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
     private static final int MAX_CAPACITY = 5;
-    private static final int CAR_1_ID = 0;
-    private static final int CAR_2_ID = 1;
-    private static final int SLOT_1_ID = 0;
-    private static final int SLOT_2_ID = 1;
+    private static final int CAR_0_ID = 0;
+    private static final int CAR_1_ID = 1;
+    private static final int SLOT_0_ID = 0;
+    private static final int SLOT_1_ID = 1;
     private static final int CAR_NOT_PRESENT_ID = -1;
     
     private ParkingLot parkingLot;
@@ -26,9 +26,20 @@ public class ParkingLotTest {
 
     @Test
     public void testParkCar() throws Exception {
-        String methodOutput = tapSystemOut(() -> this.parkingLot.parkCar(CAR_1_ID)).trim();
-        assertEquals("SLOT " + SLOT_1_ID + " is allocated to " + CAR_1_ID, methodOutput);
-        assertEquals(SLOT_1_ID, this.parkingLot.getCarIDToSlotNumberMap(CAR_1_ID));
+        String methodOutput = tapSystemOut(() -> this.parkingLot.parkCar(CAR_0_ID)).trim();
+        assertEquals("SLOT " + SLOT_0_ID + " is allocated to " + CAR_0_ID, methodOutput);
+        assertEquals(CAR_0_ID, this.parkingLot.retrieveSlotNumberToCarIdEntry(SLOT_0_ID).getValue());
+    }
+
+    @Test
+    public void testParkCarInClosestSpot() throws Exception {
+        final int SLOT_2_ID = 2;
+        final int CAR_2_ID = 2;
+        this.parkingLot.setCarIDToSlotNumber(CAR_0_ID, SLOT_0_ID);
+        this.parkingLot.setCarIDToSlotNumber(CAR_1_ID, SLOT_2_ID);
+        String methodOutput = tapSystemOut(() -> this.parkingLot.parkCar(CAR_2_ID)).trim();
+        assertEquals("SLOT " + SLOT_1_ID + " is allocated to " + CAR_2_ID, methodOutput);
+        assertEquals(CAR_2_ID, this.parkingLot.retrieveSlotNumberToCarIdEntry(SLOT_1_ID).getValue());
     }
 
     @Test
@@ -43,9 +54,9 @@ public class ParkingLotTest {
 
     @Test
     public void testFindCar() throws Exception {
-        this.parkingLot.setCarIDToSlotNumber(CAR_1_ID, SLOT_1_ID);
-        String methodOutput = tapSystemOut(() -> this.parkingLot.findCar(CAR_1_ID)).trim();
-        assertEquals(CAR_1_ID + " is parked at Slot number " + SLOT_1_ID, methodOutput);
+        this.parkingLot.setCarIDToSlotNumber(CAR_0_ID, SLOT_0_ID);
+        String methodOutput = tapSystemOut(() -> this.parkingLot.findCar(CAR_0_ID)).trim();
+        assertEquals(CAR_0_ID + " is parked at Slot number " + SLOT_0_ID, methodOutput);
     }
 
     @Test
@@ -58,13 +69,13 @@ public class ParkingLotTest {
 
     @Test
     public void testListCars() throws Exception {
+        this.parkingLot.setCarIDToSlotNumber(CAR_0_ID, SLOT_0_ID);
         this.parkingLot.setCarIDToSlotNumber(CAR_1_ID, SLOT_1_ID);
-        this.parkingLot.setCarIDToSlotNumber(CAR_2_ID, SLOT_2_ID);
         String methodOutput = tapSystemOut(() ->
-                this.parkingLot.listCars(List.of(CAR_1_ID, CAR_2_ID)))
+                this.parkingLot.listCars(List.of(CAR_0_ID, CAR_1_ID)))
                 .trim();
-        String expected = CAR_1_ID + " is parked at Slot number " + SLOT_1_ID + "\n"
-                        + CAR_2_ID + " is parked at Slot number " + SLOT_2_ID;
+        String expected = CAR_0_ID + " is parked at Slot number " + SLOT_0_ID + "\n"
+                        + CAR_1_ID + " is parked at Slot number " + SLOT_1_ID;
         assertEquals(expected, methodOutput);
     }
 
@@ -78,14 +89,22 @@ public class ParkingLotTest {
 
     @Test
     public void testUnParkCar() throws Exception {
-        this.parkingLot.setCarIDToSlotNumber(CAR_1_ID, SLOT_1_ID);
-        String methodOutput = tapSystemOut(() -> this.parkingLot.unParkCar(CAR_1_ID)).trim();
-        assertEquals("Slot " + SLOT_1_ID + " is free", methodOutput);
-        assertNull(this.parkingLot.getCarIDToSlotNumberMap(CAR_1_ID));
+        this.parkingLot.setCarIDToSlotNumber(CAR_0_ID, SLOT_0_ID);
+        String methodOutput = tapSystemOut(() -> this.parkingLot.unParkCar(CAR_0_ID)).trim();
+        assertEquals("Slot " + SLOT_0_ID + " is free", methodOutput);
+        assertNull(this.parkingLot.retrieveSlotNumberToCarIdEntry(SLOT_0_ID).getValue());
     }
 
     @Test
     public void TestUnParkACarThatIsNotInTheParkingLot() {
+        var exception = assertThrows(InvalidParameterException.class,
+                () -> this.parkingLot.unParkCar(CAR_NOT_PRESENT_ID)
+        );
+        assertEquals("Car with id: " + CAR_NOT_PRESENT_ID + " could not be found", exception.getMessage());
+    }
+
+    @Test
+    public void TestUnParkACarFromAnInvalidSlot() {
         var exception = assertThrows(InvalidParameterException.class,
                 () -> this.parkingLot.unParkCar(CAR_NOT_PRESENT_ID)
         );
